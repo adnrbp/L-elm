d78b48891e7e51480a4ab5361f1ce271d1b53924
adnrbp <adnrbp@users.noreply.github.com>
2020-04-30 00:02:02 -0500

    [view] main : define model, header component and render content from model

--- share-it/src/Main.elm
+++ share-it/src/Main.elm
@@ -3,11 +3,35 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+-- record (result) inside a record (model)
+model =
+    { result =
+        { id = 1
+        , name = "adnrbp/L-elm"
+        , stars = 66
+        }
+    }
+
 
 main =
-    div [ class "content" ]
-        [ header []
-            [ h1 [] [ text "Share it"]
-            , span [ class "tagline" ] [ text "Share code platform" ]
+    -- portable chunks of Virtual DOM nodes
+    let
+        shareItHeader =
+            header []
+                [ h1 [] [ text "Share it"]
+                , span [ class "tagline" ] [ text "Share code platform" ]
+                ]
+    in
+        div [ class "content" ]
+            [ shareItHeader
+            , ul [ class "results" ]
+                [ li []
+                    -- show stars
+                    -- [ text (toString model.result.stars)]
+                    [ span [ class "star-count" ] [ text (String.fromInt model.result.stars)]
+                    -- link to point github-account
+                    , a [ href ("https://github.com/" ++ model.result.name) ]
+                    [text model.result.name]
+                    ]
+                ]
             ]
-        ]
